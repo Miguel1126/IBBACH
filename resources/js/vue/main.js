ef5f0eb6922ffab17c7f93f2b8d8a3d0e51538e5
Kevin Chacon <kevinchacon3211@gmail.com>
2022-06-01 14:01:12 -0600

    Imported sweetalert2 & added to the App

--- resources/js/vue/main.js
+++ resources/js/vue/main.js
@@ -4,4 +4,7 @@ import { router } from './router'
 
 import 'bootstrap'
 
-createApp(App).use(router).mount('#app')
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+
+createApp(App).use(router).use(VueSweetalert2).mount('#app')
