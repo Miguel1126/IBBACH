115231677146abb78460ba0453bdf789b6775a52
Kevin Chacon <kevinchacon3211@gmail.com>
2022-06-01 00:48:31 -0600

    Deleted test components

--- resources/js/vue/components/Estas.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <main>
-        <h1 id="about-page">Hola Como Estas</h1>
-    </main>
-</template>
--- resources/js/vue/components/Hola.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <main>
-        <h1 id="about-page">Hola</h1>
-    </main>
-</template>
