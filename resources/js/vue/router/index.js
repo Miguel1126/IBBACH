0fde20aa30e2d3558ae1e4e9d70d164d05a171df
Kevin Chacon <kevinchacon3211@gmail.com>
2022-06-01 01:13:32 -0600

    Routes updated

--- resources/js/vue/router/index.js
+++ resources/js/vue/router/index.js
@@ -1,10 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router' 
-import Hola from '../components/Hola.vue'
-import Como from '../components/Como.vue'
-import Estas from '../components/Estas.vue'
-import Quetal from '../components/Quetal.vue'
 import Home from '../views/Home.vue'
-import About from '../views/About.vue'
+import Loads from '../components/admin/Loads.vue'
 
 export const router = createRouter({
   history: createWebHistory(),
@@ -14,28 +10,9 @@ export const router = createRouter({
 			component: Home
 		},
 		{
-			path: '/about',
-			component: About
-		},
-    {
-      path: '/hola',
-      name: 'Hola',
-      component: Hola
-    },
-    {
-      path: '/como',
-      name: 'Como',
-      component: Como
-    },
-    {
-      path: '/estas',
-      name: 'Estas',
-      component: Estas
-    },
-    {
-      path: '/quetal',
-      name: 'Quetal',
-      component: Quetal
+      path: '/cargas',
+      name: 'Loads',
+      component: Loads
     }
   ]
 })
