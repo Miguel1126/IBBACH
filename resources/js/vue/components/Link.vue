95b2ebc28c3f6975cece3a3ed354e751f9c6b81e
Kevin Chacon <kevinchacon3211@gmail.com>
2022-06-01 18:07:57 -0600

    Routes simplified in a component

--- resources/js/vue/components/Link.vue
+++ resources/js/vue/components/Link.vue
@@ -1,41 +1,16 @@
 <script setup>
-import Link from './Link.vue'
-import { ref } from 'vue'
-import logoURL from '../assets/logo.png'
-const is_expanded = ref(localStorage.getItem("is_expanded") === "true")
-const ToggleMenu = () => {
-	is_expanded.value = !is_expanded.value
-	localStorage.setItem("is_expanded", is_expanded.value)
-}
+    defineProps({
+        url: String,
+        name: String,
+        icon: String
+    })
 </script>
-
 <template>
-	<aside :class="`${is_expanded ? 'is-expanded' : ''}`">
-		<div class="logo">
-			<img :src="logoURL" alt="Vue" /> 
-		</div>
-
-		<div class="menu-toggle-wrap">
-			<button class="menu-toggle" @click="ToggleMenu">
-				<span class="material-icons">keyboard_double_arrow_right</span>
-			</button>
-		</div>
-
-		<h3>Menu</h3>
-		<div class="menu">
-			<Link url="/" name="Home" icon="home"></Link>
-			<Link url="/cargas" name="Cargas" icon="dataset"></Link>
-			<Link url="/grupos" name="Grupos" icon="workspaces"></Link>
-		</div>
-
-		<div class="flex"></div>
-		
-		<div class="menu">
-			<Link url="/settings" name="Settings" icon="settings"></Link>
-		</div>
-	</aside>
+    <router-link :to="url" class="button">
+		<span class="material-icons">{{ icon }}</span>
+		<span class="text">{{ name }}</span>
+	</router-link>
 </template>
-
 <style lang="scss" scoped>
 aside  {
 	display: flex;
