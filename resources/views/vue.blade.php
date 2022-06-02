41d7c45d3926c8e0363caded6c6fe64159558b7d
Kevin Chacon <kevinchacon3211@gmail.com>
2022-06-01 22:13:17 -0600

    Favicon route added

--- resources/views/vue.blade.php
+++ resources/views/vue.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="{{ asset('images/logo.jpg') }}">
     <title>IBBACH</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
