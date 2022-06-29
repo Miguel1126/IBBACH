8d22d12cd3401c8d9fbfc9a4217b3ef957504f98
Kevin Chacon <kevinchacon3211@gmail.com>
2022-06-29 16:41:28 -0600

    setup for groups migration

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GroupController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,3 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::resource('/grupos', GroupController::class);
+Route::get('/grupos/get', [GroupController::class, 'show']);
