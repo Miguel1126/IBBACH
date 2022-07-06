e7a6bd15f5ed2ddbb89e1968d0fe5112b8c3f85f
Mikkel_Ard <ragnarlothbrok1126@gmail.com>
2022-07-05 20:12:51 -0600

    Login and register controllers plus user model and users migration changes

--- database/migrations/2022_07_04_212436_create_users_table.php
+++ database/migrations/2022_07_04_212436_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('last_name');
             $table->string('code')->unique();
             $table->string('password');
             $table->string('role');
