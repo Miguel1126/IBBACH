abe253a10ce823813eb05fd992d457ec3a6ccc84
cesarluis17 <cesarescobar709@gmail.com>
2022-07-06 18:06:07 -0600

    controladores

--- routes/api.php
+++ routes/api.php
@@ -6,10 +6,13 @@ use App\Http\Controllers\ApplicantController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\GroupController;
 use App\Http\Controllers\RegisterController;
-
 use App\Http\Controllers\SubjectController;
-
 use App\Http\Controllers\LoadController;
+use App\Http\Controllers\InscriptionController;
+use App\Http\Controllers\ScheduleController;
+
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -29,8 +32,13 @@ Route::resource('/grupos', GroupController::class);
 Route::get('/grupos/get', [GroupController::class, 'show']);
 
 Route::resource('/asignaturas', SubjectController::class);
-Route::get('/asignaturas/get', [SubjectController::class, 'show']); 
-Route::post('/asignaturas/post', [SubjectController::class, 'store']); 
+Route::get('/asignaturas/get', [SubjectController::class, 'show']);
+
+Route::resource('/inscripciones', InscriptionController::class);
+Route::get('/inscripciones/get', [InscriptionController::class, 'show']); 
+
+Route::resource('/horarios', ScheduleController::class);
+Route::get('/horarios/get', [ScheduleController::class, 'show']); 
 
 Route::post('/cargas/save',[LoadController::class,'store']);
 Route::get('/cargas/all',[LoadController::class,'show']);
