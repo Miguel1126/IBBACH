279aeff3cb8ef9d5221c69f3ef619a4b62dab604
heriberto34 <alvarez2003q@gmail.com>
2022-09-06 13:31:21 -0600

    Created more factories

---
+++ database/factories/ScheduleFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Schedule>
+ */
+class ScheduleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'start_time'=> $this->faker->time(),
+            'end_time'=> $this->faker->time(),
+        ];
+    }
+}
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
@@ -18,24 +19,11 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
-            'last_name' => $this->faker->last_name(),
-            'code' => $this->faker->unique()->safeCode(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'name' => $this->faker->firstName() . $this->faker->firstName(),
+            'last_name' => $this->faker->lastName() . $this->faker->lastName(),
+            'code' => $this->faker->unique->text($maxNbChars = 7),
+            'password' => Hash::make('12345'),
+            'role' => 'docente',
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return static
-     */
-    public function unverified()
-    {
-        return $this->state(function (array $attributes) {
-            return [
-                'email_verified_at' => null,
-            ];
-        });
     }
 }
