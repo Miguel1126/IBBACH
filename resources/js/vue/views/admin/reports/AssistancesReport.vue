42157b6ca7c0a6962cc852679ae34cc57db6b757
heriberto34 <alvarez2003q@gmail.com>
2022-09-12 20:47:54 -0600

    pagination in the assistances report

--- resources/js/vue/views/admin/reports/AssistancesReport.vue
+++ resources/js/vue/views/admin/reports/AssistancesReport.vue
@@ -2,31 +2,38 @@
 import DataTable from '../../../components/DataTable.vue'
 export default {
     mounted() {
-        this.getAssistances();
-        this.getNotes();
+        this.getAssistances(1, true);
+        this.getNotes(1, true);
     },
     data() {
         return {
             notes: [],
+            paginationLinks: [],
             assistances: []
         };
     },
     methods: {
-        async getAssistances() {
+        async getAssistances(pageNumber, firstAssistances = false) {
+            if(firstAssistances) this.assistances[0] = 'loading'
+            if(typeof (pageNumber) == 'string'){
+                pageNumber = new URL(pageNumber).searchParams.getAll('page')[0]
+            }
             try {
-                const response = await this.axios.get("/api/getAsistencias");
+                const response = await this.axios.get('/api/getAssistancesReport?page=' + pageNumber);
                 if (response.status === 200) {
                     if (typeof (response.data) === "object") {
-                        this.assistances = response.data;
+                        this.assistances = response.data.data;
+                        this.paginationLinks = response.data.links 
                         console.log(response);
                     }
                     else {
-                        console.log(response);
+                        this.assistances[0] = 'error'
                     }
                 }
             }
             catch (error) {
                 console.error(error);
+                this.assistances[0] = 'error'
             }
         },
         async getNotes() {
@@ -66,6 +73,16 @@ export default {
 
             ]" :items="assistances">
             </DataTable>
+            <nav aria-label="Page navigation example" v-if="paginationLinks.length">
+                <ul class="pagination">
+                    <li class="page-item cursor-pointer" :class="page.active ? 'active' : ''"
+                        v-for="page in paginationLinks" :key="page">
+                        <span class="page-link" @click.prevent="getAssistances(page.url)">{{ page.label == 'pagination.previous'
+                                ? '&laquo;' : page.label == 'pagination.next' ? '&raquo;' : page.label
+                        }}</span>
+                    </li>
+                </ul>
+                </nav>
         </section>
     </main>
 </template>
