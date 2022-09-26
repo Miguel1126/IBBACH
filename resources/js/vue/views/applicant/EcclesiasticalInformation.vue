f6b233c4afa9c3728be8d093564713b355fd5004
Mikkel <ragnarlothbrok1126@gmail.com>
2022-09-26 11:56:22 -0600

    Styling the applicant form

--- resources/js/vue/views/applicant/EcclesiasticalInformation.vue
+++ resources/js/vue/views/applicant/EcclesiasticalInformation.vue
@@ -136,119 +136,119 @@ export default {
     <section class="w-100 text-black">
         <h3 class="fw-bold text-center mb-5 section-title">Datos Eclesiásticos</h3>
         <div class="questions-container">
-            <div class="mb-3">
-                <label class="form-label">¿Es pastor?</label>
-                <select class="form-select" v-model="ecclesiasticalInfo.is_pastor">
-                    <option selected>-- --</option>
-                    <option value="1">Si</option>
-                    <option value="0">No</option>
-                </select>
-                <label class="form-label">¿Es Miembro?</label>
-                <select class="form-select" v-model="ecclesiasticalInfo.is_member">
-                    <option selected>-- --</option>
-                    <option value="1">Si</option>
-                    <option value="0">No</option>
-                </select>
-            </div>
-            <div class="mb-3">
-                <label for="churchName" class="form-label">¿De qué iglesia?</label>
-                <input type="text" class="form-control" id="churchName" placeholder="Nombre"
-                    v-model="ecclesiasticalInfo.church_name" required>
-            </div>
-            <div class="mb-3">
-                <label for="churchAddress" class="form-label">Dirección de la iglesia</label>
-                <input type="text" class="form-control" id="churchAddress" placeholder=" municipio, departamento"
-                    v-model="ecclesiasticalInfo.church_address" required>
-            </div>
-            <div class="mb-3">
-                <label for="phone2" class="form-label">Teléfono de la iglesia</label>
-                <input type="text" class="form-control" id="phone2" placeholder="6458-5955"
-                    v-model="ecclesiasticalInfo.church_phone" required>
-            </div>
-            <div class="mb-3">
-                <label for="district" class="form-label">Distrito</label>
-                <input type="text" class="form-control" id="district" placeholder="Distrito"
-                    v-model="ecclesiasticalInfo.district" required>
-            </div>
-            <div class="mb-3" v-if="ecclesiasticalInfo.is_member === '1'">
-                <label for="MemOrPas" class="form-label">Si es miembro escriba el nombre de su pastor</label>
-                <input type="text" class="form-control" id="MemOrPas" placeholder="Nombre"
-                    v-model="ecclesiasticalInfo.pastor_name" required>
-            </div>
-            <div class="d-flex" v-if="ecclesiasticalInfo.is_member === '1'">
-                <div>
-                    <label for="licence" class="form-label">Licencia</label>
-                    <input type="text" class="form-control" id="licence" placeholder="licencia"
-                        v-model="ecclesiasticalInfo.licence" required>
+            <!-- start 1st container -->
+            <div class="row g-3">
+                <div class="col-6 col-md-4">
+                    <label class="form-label ">¿Es pastor?</label>
+                    <select class="form-select" v-model="ecclesiasticalInfo.is_pastor">
+                        <option selected>-- --</option>
+                        <option value="1">Si</option>
+                        <option value="0">No</option>
+                    </select>
                 </div>
-                <div class="mb-3">
-                    <label for="phone3" class="form-label">Teléfono</label>
-                    <input type="text" class="form-control" id="phone3" placeholder="6458-5955"
-                        v-model="ecclesiasticalInfo.pastor_phone" required>
+                <div class="col-6 col-md-4">
+                    <label class="form-label">¿Es Miembro?</label>
+                    <select class="form-select" v-model="ecclesiasticalInfo.is_member">
+                        <option selected>-- --</option>
+                        <option value="1">Si</option>
+                        <option value="0">No</option>
+                    </select>
                 </div>
-            </div>
-            <br />
-            <div class="mb-2 mt-3">
-                <label for="nationality" class="form-label">Anote el nombre y teléfono de dos pastores de Las Asambleas
-                    de Dios que puedan dar referencias suyas</label>
-            </div>
-            <div class="d-flex">
-                <div>
+                <div class="col-6 col-md-4">
+                    <label for="churchName" class="form-label">¿De qué iglesia?</label>
+                    <input type="text" class="form-control" id="churchName" placeholder="Nombre"
+                        v-model="ecclesiasticalInfo.church_name" required>
+                </div>
+                <div class="col-6 col-md-4">
+                    <label for="churchAddress" class="form-label">Dirección de la iglesia</label>
+                    <input type="text" class="form-control" id="churchAddress" placeholder=" municipio, departamento"
+                        v-model="ecclesiasticalInfo.church_address" required>
+                </div>
+                <div class="col-6 col-md-4">
+                    <label for="phone2" class="form-label">Teléfono de la iglesia</label>
+                    <input type="text" class="form-control" id="phone2" placeholder="6458-5955"
+                        v-model="ecclesiasticalInfo.church_phone" required>
+                </div>
+                <div class="col-6 col-md-4">
+                    <label for="district" class="form-label">Distrito</label>
+                    <input type="text" class="form-control" id="district" placeholder="Distrito"
+                        v-model="ecclesiasticalInfo.district" required>
+                </div>
+                <div class="row g-3" v-if="ecclesiasticalInfo.is_member === '1'">
+                    <div class="col-6 col-md-4" v-if="ecclesiasticalInfo.is_member === '1'">
+                        <label for="MemOrPas" class="form-label">Si es miembro escriba el nombre de su pastor</label>
+                        <input type="text" class="form-control" id="MemOrPas" placeholder="Nombre"
+                            v-model="ecclesiasticalInfo.pastor_name" required>
+                    </div>
+                    <div class="col-6 col-md-4">
+                        <label for="licence" class="form-label">Licencia</label>
+                        <input type="text" class="form-control" id="licence" placeholder="licencia"
+                            v-model="ecclesiasticalInfo.licence" required>
+                    </div>
+                    <div class="col-6 col-md-4">
+                        <label for="phone3" class="form-label">Teléfono</label>
+                        <input type="text" class="form-control" id="phone3" placeholder="6458-5955"
+                            v-model="ecclesiasticalInfo.pastor_phone" required>
+                    </div>
+                </div>
+                <div class="text-left">
+                    <label for="nationality" class="form-label">Anote el nombre y teléfono de dos pastores de
+                        las Asambleas de Dios que puedan dar referencias suyas</label>
+                </div>
+                <div class="col-6 col-md-4">
                     <label for="pastor1" class="form-label">Nombre del Pastor</label>
                     <input type="text" class="form-control" id="pastor1" placeholder="Nombre"
                         v-model="ecclesiasticalInfo.reference_name_one" required>
                 </div>
-                <div class="mb-3">
+                <div class="col-6 col-md-4">
                     <label for="phone4" class="form-label">Teléfono</label>
                     <input type="text" class="form-control" id="phone4" placeholder="6458-5955"
                         v-model="ecclesiasticalInfo.reference_phone_one" required>
                 </div>
-            </div>
-            <div class="d-flex">
-                <div class="mb-3">
+                <div class="col-6 col-md-4">
                     <label for="pastor2" class="form-label">Nombre del Pastor</label>
                     <input type="text" class="form-control" id="pastor2" placeholder="Nombre"
                         v-model="ecclesiasticalInfo.reference_name_two" required>
                 </div>
-                <div class="mb-3">
+                <div class="col-6 col-md-4">
                     <label for="phone5" class="form-label">Teléfono</label>
                     <input type="text" class="form-control" id="phone5" placeholder="6458-5955"
                         v-model="ecclesiasticalInfo.reference_phone_two" required>
                 </div>
-            </div>
-            <br />
-            <div class="mb-3">
-                <label for="lgsus" class="form-label">¿Cuando aceptó a Jesucristo?</label>
-                <textarea class="form-control" id="lgsus" placeholder="Aproximado"
-                    v-model="ecclesiasticalInfo.christ_accepted" required></textarea>
-            </div>
-            <div class="mb-3">
-                <label for="christeningDate" class="form-label">¿Cuando se bautizó?</label>
-                <input type="date" class="form-control" id="christeningDate"
-                    v-model="ecclesiasticalInfo.christening_date" required>
-            </div>
-            <div class="mb-3">
-                <label for="timeBeingMember" class="form-label">¿Cuanto tiempo tiene de ser miembro de su
-                    iglesia?</label>
-                <input type="text" class="form-control" id="timeBeingMember" placeholder="Aproximado"
-                    v-model="ecclesiasticalInfo.time_being_member" required>
-            </div>
-            <div class="mb-3">
-                <label for="privilegesHeld" class="form-label">¿Cuales son los privilegios en que se ha
-                    desempeñado?</label>
-                <textarea type="text" class="form-control" id="privilegesHeld" placeholder="Aproximado"
-                    v-model="ecclesiasticalInfo.privileges_held" required></textarea>
-            </div>
-            <div class="mb-3">
-                <label for="denomination" class="form-label">¿A qué denominación pertenece?</label>
-                <input type="text" class="form-control" id="denomination" placeholder="Aproximado"
-                    v-model="ecclesiasticalInfo.denomination" required>
-            </div>
-            <div class="mb-3">
-                <label for="studyReason" class="form-label">¿Cuál es el interés de estudiar en el Instituto Bíblico
-                    Betel Anexo? </label>
-                <textarea type="text" class="form-control" id="studyReason" placeholder="Aproximado"
-                    v-model="ecclesiasticalInfo.study_reason" required></textarea>
+
+                <div class="col-6 col-md-4">
+                    <label for="lgsus" class="form-label">¿Cuando aceptó a Jesucristo?</label>
+                    <textarea class="form-control" id="lgsus" placeholder="Aproximado"
+                        v-model="ecclesiasticalInfo.christ_accepted" required></textarea>
+                </div>
+                <div class="col-6 col-md-4">
+                    <label for="christeningDate" class="form-label">¿Cuando se bautizó?</label>
+                    <input type="date" class="form-control" id="christeningDate"
+                        v-model="ecclesiasticalInfo.christening_date" required>
+                </div>
+                <div class="col-6 col-md-4">
+                    <label for="privilegesHeld" class="form-label">¿Cuales son los privilegios en que se ha
+                        desempeñado?</label>
+                    <textarea type="text" class="form-control" id="privilegesHeld" placeholder="Aproximado"
+                        v-model="ecclesiasticalInfo.privileges_held" required></textarea>
+                </div>
+                <div class="col-6 col-md-4">
+                    <label for="timeBeingMember" class="form-label">¿Cuanto tiempo tiene de ser miembro de su
+                        iglesia?</label>
+                    <input type="text" class="form-control" id="timeBeingMember" placeholder="Aproximado"
+                        v-model="ecclesiasticalInfo.time_being_member" required>
+                </div>
+                <div class="col-6 col-md-4">
+                    <label for="studyReason" class="form-label">¿Cuál es el interés de estudiar en el Instituto Bíblico
+                        Betel Anexo? </label>
+                    <textarea type="text" class="form-control" id="studyReason" placeholder="Aproximado"
+                        v-model="ecclesiasticalInfo.study_reason" required></textarea>
+                </div>
+                <div class="col-6 col-md-4">
+                    <label for="denomination" class="form-label">¿A qué denominación pertenece?</label>
+                    <input type="text" class="form-control" id="denomination" placeholder="Aproximado"
+                        v-model="ecclesiasticalInfo.denomination" required>
+                </div>
             </div>
         </div>
     </section>
@@ -258,11 +258,6 @@ export default {
     text-shadow: 2px 2px 3px rgb(0, 0, 0);
 }
 
-.form-container {
-    background: var(--bs-gray-400);
-    border: 6px solid #ccc;
-}
-
 .section-title {
     text-decoration: underline;
 }
@@ -270,5 +265,8 @@ export default {
 .questions-container {
     display: grid;
     justify-content: left;
+}
+.col-6{
+    min-width: 300px;
 }
 </style>
--- resources/js/vue/views/applicant/PersonalInformation.vue
+++ resources/js/vue/views/applicant/PersonalInformation.vue
@@ -63,65 +63,67 @@ export default {
     <section class="w-100 text-black">
         <h3 class="fw-bold text-center mb-5 section-title">Datos Personales</h3>
         <div class="questions-container">
-            <div class="mb-3">
-                <label for="name" class="form-label">Nombres</label>
-                <input type="text" class="form-control" id="name" placeholder="Juan José" v-model="personalInfo.name"
-                    required>
-            </div>
-            <div class="mb-3">
-                <label for="lastname" class="form-label">Apellidos</label>
-                <input type="text" class="form-control" id="lastname" placeholder="Perez López"
-                    v-model="personalInfo.last_name" required>
-            </div>
-            <div class="mb-3">
-                <label for="email" class="form-label">Correo electrónico</label>
-                <input type="email" class="form-control" id="email" placeholder="juanperez@gmail.com"
-                    v-model="personalInfo.email" required>
-            </div>
-            <div class="mb-3">
-                <label for="phone1" class="form-label">Teléfono</label>
-                <input type="text" class="form-control" id="phone1" placeholder="6458-5955" v-model="personalInfo.phone"
-                    required>
-            </div>
-            <div class="mb-3">
-                <label for="address" class="form-label">Dirección donde reside</label>
-                <input type="text" class="form-control" id="address"
-                    placeholder="Barrio el centro, Chalatenango, Chalatenango, El Salvador"
-                    v-model="personalInfo.address" required>
-            </div>
-            <div class="mb-3">
-                <label for="nationality" class="form-label">Nacionalidad</label>
-                <input type="text" class="form-control" id="nationality" placeholder="Salvadoreñe"
-                    v-model="personalInfo.nationality" required>
-            </div>
-            <div class="mb-3">
-                <label for="birthDate" class="form-label">Fecha de nacimiento</label>
-                <input type="date" class="form-control" id="birthDate" v-model="personalInfo.birth_date" required>
-            </div>
-            <div class="mb-3">
-                <label class="form-label">Estado civil</label>
-                <select class="form-select" v-model="personalInfo.marital_status">
-                    <option selected>-- --</option>
-                    <option value="Soltero/a">Soltero/a</option>
-                    <option value="Casado/a">Casado/a</option>
-                    <option value="Divorsiado/a">Divorciado/a</option>
-                    <option value="Viudo/a">Viudo/a</option>
-                </select>
-            </div>
-            <div class="mb-3" v-if="personalInfo.marital_status === 'Casado/a'">
-                <label for="mateName" class="form-label">Nombre del cónyuge</label>
-                <input type="text" class="form-control" id="mateName" placeholder="Juan Perez"
-                    v-model="personalInfo.mate_name" required>
-            </div>
-            <div class="mb-3">
-                <label for="secularDegree" class="form-label">Último grado secular estudiado</label>
-                <input type="text" class="form-control" id="secularDegree" placeholder="Bachillerato stop"
-                    v-model="personalInfo.secular_degree" required>
-            </div>
-            <div class="mb-3">
-                <label for="currentOcupation" class="form-label">Ocupación actual</label>
-                <input type="text" class="form-control" id="currentOcupation" placeholder="Corralero"
-                    v-model="personalInfo.current_ocupation" required>
+            <div class="row g-3">
+                <div class="col-6 col-md-4">
+                    <label for="name" class="form-label">Nombres</label>
+                    <input type="text" class="form-control" id="name" placeholder="Juan José"
+                        v-model="personalInfo.name" required>
+                </div>
+                <div class="col-6 col-md-4">
+                    <label for="lastname" class="form-label">Apellidos</label>
+                    <input type="text" class="form-control" id="lastname" placeholder="Perez López"
+                        v-model="personalInfo.last_name" required>
+                </div>
+                <div class="col-6 col-md-4">
+                    <label for="email" class="form-label">Correo electrónico</label>
+                    <input type="email" class="form-control" id="email" placeholder="juanperez@gmail.com"
+                        v-model="personalInfo.email" required>
+                </div>
+                <div class="col-6 col-md-4">
+                    <label for="phone1" class="form-label">Teléfono</label>
+                    <input type="text" class="form-control" id="phone1" placeholder="6458-5955"
+                        v-model="personalInfo.phone" required>
+                </div>
+                <div class="col-6 col-md-4">
+                    <label for="address" class="form-label">Dirección donde reside</label>
+                    <input type="text" class="form-control" id="address"
+                        placeholder="Barrio el centro, Chalatenango, Chalatenango, El Salvador"
+                        v-model="personalInfo.address" required>
+                </div>
+                <div class="col-6 col-md-4">
+                    <label for="nationality" class="form-label">Nacionalidad</label>
+                    <input type="text" class="form-control" id="nationality" placeholder="Salvadoreñe"
+                        v-model="personalInfo.nationality" required>
+                </div>
+                <div class="col-6 col-md-4">
+                    <label for="birthDate" class="form-label">Fecha de nacimiento</label>
+                    <input type="date" class="form-control" id="birthDate" v-model="personalInfo.birth_date" required>
+                </div>
+                <div class="col-6 col-md-4">
+                    <label class="form-label">Estado civil</label>
+                    <select class="form-select" v-model="personalInfo.marital_status">
+                        <option selected>-- --</option>
+                        <option value="Soltero/a">Soltero/a</option>
+                        <option value="Casado/a">Casado/a</option>
+                        <option value="Divorsiado/a">Divorciado/a</option>
+                        <option value="Viudo/a">Viudo/a</option>
+                    </select>
+                </div>
+                <div class="col-6 col-md-4" v-if="personalInfo.marital_status === 'Casado/a'">
+                    <label for="mateName" class="form-label">Nombre del cónyuge</label>
+                    <input type="text" class="form-control" id="mateName" placeholder="Juan Perez"
+                        v-model="personalInfo.mate_name" required>
+                </div>
+                <div class="col-6 col-md-4">
+                    <label for="secularDegree" class="form-label">Último grado secular estudiado</label>
+                    <input type="text" class="form-control" id="secularDegree" placeholder="Bachillerato stop"
+                        v-model="personalInfo.secular_degree" required>
+                </div>
+                <div class="col-6 col-md-4">
+                    <label for="currentOcupation" class="form-label">Ocupación actual</label>
+                    <input type="text" class="form-control" id="currentOcupation" placeholder="Corralero"
+                        v-model="personalInfo.current_ocupation" required>
+                </div>
             </div>
         </div>
     </section>
@@ -143,5 +145,9 @@ export default {
 .questions-container {
     display: grid;
     justify-content: left;
+}
+
+.col-6{
+    min-width: 300px;
 }
 </style>
