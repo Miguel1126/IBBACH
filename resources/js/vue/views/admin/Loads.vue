50183be1c12535d90eabcd3717010fccc19b85c4
Kevin Chacon <kevinchacon3211@gmail.com>
2022-09-30 18:26:39 -0600

    loads

--- resources/js/vue/views/admin/Loads.vue
+++ resources/js/vue/views/admin/Loads.vue
@@ -1,14 +1,15 @@
 <script setup>
 import { handleErrors } from '../../js/handle_error'
+import { formatTime } from '../../js/format_time'
 import DataTable from '../../components/DataTable.vue'
 </script>
 <script>
 export default {
     mounted() {
-        this.getCycles(1, true);
-        this.getSubjects(1, true);
-        this.getTeacher(1, true);
-        this.getSchedules(1, true);
+        this.getCycles(1, true)
+        this.getSubjects(1, true)
+        this.getTeacher(1, true)
+        this.getSchedules(1, true)
         this.getLoads(1, true)
     },
     data() {
@@ -19,31 +20,31 @@ export default {
             schedules: [],
             loads: [],
             paginationLinks: [],
-            cySelected: [],
-            subSelected: [],
-            teaSelected: [],
-            schSelected: [],
+            cySelected: '',
+            subSelected: '',
+            teaSelected: '',
+            schSelected: '',
             editing: false,
             tableHeaders: [
-                {title: 'Id'},
-                {title: 'Estado'},
-                {title: 'Ciclo'},
-                {title: 'Asignatura'},
-                {title: 'Docente'},
-                {title: 'Apellidos'},
-                {title: 'Hora de inicio'},
-                {title: 'Hora de fin'},
-                {title: 'Acciones'}
+                { title: 'Id' },
+                { title: 'Estado' },
+                { title: 'Ciclo' },
+                { title: 'Asignatura' },
+                { title: 'Docente' },
+                { title: 'Apellidos' },
+                { title: 'Hora de inicio' },
+                { title: 'Hora de fin' },
+                { title: 'Acciones' }
             ],
         }
     },
     methods: {
         async getLoads(pageNumber, firtsLoad = false) {
-            if(firtsLoad) this.loads[0] = 'loading'
+            if (firtsLoad) this.loads[0] = 'loading'
 
             if (typeof (pageNumber) == 'string') {
-                    pageNumber = new URL(pageNumber).searchParams.getAll('page')[0]
-                }
+                pageNumber = new URL(pageNumber).searchParams.getAll('page')[0]
+            }
             try {
                 this.loads[0] = 'loading'
                 const response = await this.axios.get('/api/getCargas?page=' + pageNumber);
@@ -83,7 +84,7 @@ export default {
                 const response = await this.axios.get('/api/getAsignaturas')
                 if (response.status === 200) {
                     if (typeof (response.data) === 'object') {
-                        this.subjects = response.data
+                        this.subjects = response.data.data
                     }
                     else {
                         console.log(response)
@@ -99,7 +100,7 @@ export default {
                 const response = await this.axios.get('/api/getDocentes')
                 if (response.status === 200) {
                     if (typeof (response.data) === 'object') {
-                        this.teachers = response.data
+                        this.teachers = response.data.data
                     }
                     else {
                         console.log(response)
@@ -115,7 +116,7 @@ export default {
                 const response = await this.axios.get('/api/getHorarios')
                 if (response.status === 200) {
                     if (typeof (response.data) === 'object') {
-                        this.schedules = response.data
+                        this.schedules = response.data.data
                     }
                     else {
                         console.log(response)
@@ -212,14 +213,14 @@ export default {
             }
         },
         selectLoad(event, load) {
-           /*  this.clearDropdown()
-            this.editing = true
-            this.cySelected.push(load)
-            this.subSelected.push(load)
-            this.teaSelected.push(load)
-            this.schSelected.push(load)
-            console.log(this.subSelected)
-            console.log(this.teaSelected) */
+            /*  this.clearDropdown()
+             this.editing = true
+             this.cySelected.push(load)
+             this.subSelected.push(load)
+             this.teaSelected.push(load)
+             this.schSelected.push(load)
+             console.log(this.subSelected)
+             console.log(this.teaSelected) */
             console.log(load)
         },
         saveEdit() {
@@ -267,7 +268,7 @@ export default {
         this.getSchedules()
         this.getLoads()
     },
-    components: {DataTable}
+    components: { DataTable }
 }
 </script>
 
@@ -277,89 +278,75 @@ export default {
         <br />
         <section class="p-3">
             <h3 class="h3 fw-semibold">Asignar una nueva carga</h3>
-            <div class="d-flex flex-wrap">
-                <div class="form-control">
-                    <select class="form-select" id="roles" v-model="roleSelected" required>
-                        <!--<option v-for="cycle in cycles" :key="cycle.id" :value="cycle.cycle">{{ cycle.cycle }}
-                        </option>-->
-                    </select>
+            <form>
+                <div class="d-flex flex-wrap">
+                    <div class="select-input">
+                        <select class="form-select select-input" v-model="cySelected">
+                            <option selected value="">Selecciona un ciclo</option>
+                            <option v-for="cycle in cycles" :key="cycle.id" :value="cycle.id">{{ cycle.cycle }}</option>
+                        </select>
+                    </div>
+                    <div class="select-input">
+                        <select class="form-select select-input" v-model="subSelected">
+                            <option selected value="">Selecciona una materia</option>
+                            <option v-for="subject in subjects" :key="subject.id" :value="subject.id">{{ subject.subject
+                            }}</option>
+                        </select>
+                    </div>
+                    <div class="select-input">
+                        <select class="form-select select-input" v-model="teaSelected">
+                            <option selected value="">Selecciona un docente</option>
+                            <option v-for="teacher in teachers" :key="teacher.id" :value="teacher.id">{{ teacher.name }}
+                                {{ teacher.last_name }}</option>
+                        </select>
+                    </div>
+                    <div class="select-input">
+                        <select class="form-select select-input" v-model="schSelected">
+                            <option selected value="">Selecciona un horario</option>
+                            <option v-for="schedule in schedules" :key="schedule.id" :value="schedule.id">{{
+                            formatTime(schedule.start_time) }} - {{ formatTime(schedule.end_time) }}</option>
+                        </select>
+                    </div>
                 </div>
-                <div class="dropdown m-4">
-                    <button class="btn btn-secondary btn-lg dropdown-toggle" type="button" id="dropdownMenuButton2"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                        <span v-if="!subSelected.length">Asignaturas</span>
-                        <span v-else>{{ subSelected[0].subject }}</span>
-                    </button>
-                    <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
-                       <!-- <li v-for="subject in subjects" :key="subject.id" class="dropdown-item text-light list-click"
-                            @click="selectSub($event, subject)">{{ subject.subject }}</li>-->
-                    </ul>
-                </div>
-                <div class="dropdown m-4">
-                    <button class="btn btn-secondary btn-lg dropdown-toggle" type="button" id="dropdownMenuButton2"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                        <span v-if="!teaSelected.length">Docente</span>
-                        <span v-else>{{ teaSelected[0].teacher }}</span>
-                    </button>
-                    <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
-                       <!-- <li v-for="teacher in teachers" :key="teacher.id" class="dropdown-item text-light list-click"
-                            @click="selectTea($event, teacher)">{{ teacher.teacher }}</li>-->
-                    </ul>
-                </div>
-                <div class="dropdown m-4">
-                    <button class="btn btn-secondary btn-lg dropdown-toggle" type="button" id="dropdownMenuButton2"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                        <span v-if="!schSelected.length">Horario</span>
-                        <span v-else>{{ schSelected[0].start_time || loads.schedule }} - {{ schSelected[0].end_time ||
-                                loads.schedule
-                        }}</span>
-                    </button>
-                    <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
-                        <!--<li v-for="schedule in schedules" :key="schedule.id" class="dropdown-item text-light list-click"
-                            @click="selectSch($event, schedule)">{{ schedule.start_time }} - {{ schedule.end_time }}
-                        </li>-->
-                    </ul>
-                </div>
-                <div class="m-4">
-                    <button v-if="!editing" type="button" class="d-inline-flex btn btn-primary btn-lg" id="adder-btn"
-                        @click="saveLoad">Agregar <i class="material-icons m-auto ms-1">add_box</i></button>
-                    <button v-else type="button" class="d-inline-flex btn btn-success btn-lg" @click="saveEdit">Guardar
-                        <i class="material-icons m-auto ms-1">edit</i></button>
-                    <button v-if="!editing" type="button" class="d-inline-flex btn btn-warning btn-lg ms-3"
-                        @click="clearDropdown">Limpiar <i class="material-icons m-auto ms-1">backspace</i></button>
-                    <button v-else type="button" class="d-inline-flex btn btn-danger btn-lg ms-3"
-                        @click="clearDropdown">Cancelar <i class="material-icons m-auto ms-1">cancel</i></button>
-                </div>
-            </div>
+            </form>
         </section>
         <hr class="separator" />
         <section class="p-3">
-            <DataTable
-            title="Listado de cargas"
-            :headers="tableHeaders"
-            :items="loads"
-            >
+            <DataTable title="Listado de cargas" :headers="tableHeaders" :items="loads">
                 <template #actions="{ item }">
-                    <button type="button" class="btn btn-primary me-2" @click="selectLoad($event, item)">Modificar</button>
-                    <button type="button" class="btn btn-danger" @click="confirmDelete($event, load.id)">Eliminar</button>
+                    <button type="button" class="btn btn-primary me-2"
+                        @click="selectLoad($event, item)">Modificar</button>
+                    <button type="button" class="btn btn-danger"
+                        @click="confirmDelete($event, load.id)">Eliminar</button>
                 </template>
             </DataTable>
             <nav aria-label="Page navigation example" v-if="paginationLinks.length">
-                    <ul class="pagination">
-                        <li class="page-item cursor-pointer" :class="page.active ? 'active' : ''"
-                            v-for="page in paginationLinks" :key="page">
-                            <span class="page-link" @click="getLoads(page.url)">{{ page.label == 'pagination.previous'
-                                    ? '&laquo;' : page.label == 'pagination.next' ? '&raquo;' : page.label
-                            }}</span>
-                         </li>
-                    </ul>
-                </nav>
+                <ul class="pagination">
+                    <li class="page-item cursor-pointer" :class="page.active ? 'active' : ''"
+                        v-for="page in paginationLinks" :key="page">
+                        <span class="page-link" @click="getLoads(page.url)">{{ page.label == 'pagination.previous'
+                        ? '&laquo;' : page.label == 'pagination.next' ? '&raquo;' : page.label
+                        }}</span>
+                    </li>
+                </ul>
+            </nav>
         </section>
     </main>
 </template>
 
 <style scoped>
+.select-input {
+    min-width: 300px;
+    margin: 2px;
+}
+
 .list-click {
     cursor: pointer;
+}
+
+@media (max-width: 377px) {
+    .select-input {
+        min-width: 200px;
+    }
 }
 </style>
