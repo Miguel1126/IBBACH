7f23efa19fbaea449df37e930ad046761a66b69e
Kevin Chacon <kevinchacon3211@gmail.com>
2022-10-07 12:27:22 -0600

    time formatter fixed

--- resources/js/vue/js/format_time.js
+++ resources/js/vue/js/format_time.js
@@ -1,6 +1,6 @@
 export const formatTime = (time) => {
     let timeSplit = time.split(":")
-    let amOrPm = timeSplit[0] > 12 ? "PM" : "AM"
+    let amOrPm = timeSplit[0] >= 12 ? "PM" : "AM"
     let hour = timeSplit[0] > 12 ? timeSplit[0] - 12 : timeSplit[0]
 
     return `${hour}:${timeSplit[1]} ${amOrPm}`
