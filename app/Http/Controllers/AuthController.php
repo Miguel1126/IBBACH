b84a53f9b886ffa10b40a90d435c1ceec5ae2871
Kevin Chacon <kevinchacon3211@gmail.com>
2022-10-07 18:11:57 -0600

    method updated to filter inactive users

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,34 +10,51 @@ use Laravel\Sanctum\PersonalAccessToken;
 
 class AuthController extends Controller
 {
-    public function login(Request $request){
-        $user = User::where("code", "=", $request->code)->first();
-        if (isset($user->id)) {
-            if(Hash::check($request->password, $user->password)) {
-                $token = $user->createToken("auth_token")->plainTextToken;
-                return response()->json([
-                    "status" => "logged",
-                    "role" => $user->role,
-                    "token" => $token
-                ],200);
-            }
-            else {
+    public function login(Request $request)
+    {
+        try {
+            $user = User::where("code", "=", $request->code)->first();
+            if ($user->status === "inactivo") {
                 return response()->json([
                     "status" => "notlogged",
                     "message" => "El usuario o la contraseña son incorrectos"
                 ], 200);
             }
-        }
-        else {
-            return response()->json([
-                "status" => "notlogged",
-                "message" => "El usuario o la contraseña son incorrectos"
-            ], 200);
+            if (isset($user->id)) {
+                if (Hash::check($request->password, $user->password)) {
+                    $token = $user->createToken("auth_token")->plainTextToken;
+                    return response()->json([
+                        "status" => "logged",
+                        "role" => $user->role,
+                        "token" => $token
+                    ], 200);
+                } else {
+                    return response()->json([
+                        "status" => "notlogged",
+                        "message" => "El usuario o la contraseña son incorrectos"
+                    ], 200);
+                }
+            } else {
+                return response()->json([
+                    "status" => "notlogged",
+                    "message" => "El usuario o la contraseña son incorrectos"
+                ], 200);
+            }
+        } catch (\Exception $e) {
+            return response()->json(["message" => $e->getMessage()], 500);
         }
     }
 
-    public function getLoggedUser() {
+    public function getLoggedUser(Request $request)
+    {
         try {
+            if (auth()->user()->status !== 'activo') {
+                $accessToken = $request->bearerToken();
+                $token = PersonalAccessToken::findToken($accessToken);
+                $token->delete();
+                return response()->json(['message' => 'No tienes autorización para ejecutar esta acción, inicia sesión en una cuenta válida'], 401);
+            }
+
             return response()->json([
                 'id' => auth()->user()->id,
                 'name' => auth()->user()->name,
@@ -46,22 +63,21 @@ class AuthController extends Controller
                 'role' => auth()->user()->role,
                 'status' => auth()->user()->status,
                 'applicant_id' => auth()->user()->applicant_id,
-            ],200);
-        }
-        catch (\Exception $ex) {
-            return response()->json([$ex->getMessage()],500);
+            ], 200);
+        } catch (\Exception $ex) {
+            return response()->json(["message" => $ex->getMessage()], 500);
         }
     }
 
-    public function logout(Request $request) {
+    public function logout(Request $request)
+    {
         try {
             $accessToken = $request->bearerToken();
             $token = PersonalAccessToken::findToken($accessToken);
             $token->delete();
             $success = true;
             $message = "Se cerró la sesión correctamente";
-        }
-        catch (\Exception $ex) {
+        } catch (\Exception $ex) {
             $success = false;
             $message = $ex->getMessage();
         }
