c9b5be2db620426712edfa6e2b87e64640fb4d74
heriberto34 <alvarez2003q@gmail.com>
2022-10-22 21:31:32 -0600

    modified home page

--- resources/js/vue/views/Home.vue
+++ resources/js/vue/views/Home.vue
@@ -1,5 +1,7 @@
 <script>
-import Nav from '../components/Nav.vue';
+
+
+
 
 
 
@@ -17,8 +19,8 @@ export default {
 		}
 	},
 	components: {
-    Nav,
-},
+
+	},
 	setup() {
 		document.title = "IBBACH | Home"
 
@@ -46,25 +48,34 @@ export default {
 }
 </script>
 <template>
-	<nav class="navbar navbar-expand-lg shadow">
-		<div class="">
-			<img class=" img-fluid logo-image" :src="`${siteUrl}/images/logo.jpg`" />
-		</div>
-		<div class="d-flex gap-2 me-2">
-			<router-link class="links" to="/login">Iniciar Sesión</router-link>
-			<router-link class="links" to="/aplicante/formulario">Solicitud de Ingreso</router-link>
-			<router-Link class="links" to="/ProgramaEstudio">Programa de estudio</router-Link>
-		</div>
-	</nav>
+	<nav class="navbar navbar-expand-lg ">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="http://127.0.0.1:8000/"><img class=" img-fluid logo-image" :src="`${siteUrl}/images/logo.jpg`" /></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item m-1">
+			<router-link class="links link" to="/login">Iniciar Sesión</router-link>
+        </li>
+        <li class="nav-item m-1">
+			<router-link class="links link" to="/aplicante/formulario">Solicitud de Ingreso</router-link>
+        </li>
+        <li class="nav-item m-1">
+			<router-Link class="links link" to="/ProgramaEstudio">Programa de estudio</router-Link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 	<div class="container">
 		<h1 class="text-center my-4">Instituto Bíblico Betel Anexo Chalatenango</h1>
 	</div>
-	<div id="demo" class="carousel slide m-4" data-bs-ride="carousel">
-
+	<div id="demo" class="carousel slide m-4" data-bs-ride="carousel"  >
 		<div class="carousel-inner">
-			<div v-for="publication in publications" class="carousel-item active">
-				<img :src="`${siteUrl}/images/publications/${publication.img}`" class="mx-auto img-fluid  d-block"
-					style="width: 25%; height: 20%" />
+			<div v-for="publication in publications" class="carousel-item active"   >
+				<img :src="`${siteUrl}/images/publications/${publication.img}`" class="img mx-auto img-fluid  d-block" style=" min-width: 200px; min-height: 300px; max-width: 250px; max-height: 350px;"/>
 			</div>
 		</div>
 		<button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
@@ -73,9 +84,6 @@ export default {
 		<button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
 			<span class="carousel-control-next-icon"></span>
 		</button>
-	</div>
-	<div>
-
 	</div>
 	<div class="row m-3">
 		<div class="col-sm-6">
@@ -109,7 +117,7 @@ export default {
 			<div class="row row-30">
 				<div class="col-md-4 col-xl-5">
 					<div class="pr-xl-4"><a class="brand" href="http://127.0.0.1:8000/"><img
-								class=" d-block img-fluid rounded mt-3" style="width: 20%; height: 20%"
+								class=" d-block img-fluid rounded mt-3" style="width: 20%; height: 20%;"
 								:src="`${siteUrl}/images/logo.jpg`" /></a>
 						<br>
 						<h6> El Instituto Bíblico Betel Anexo Chalatenango, inició operaciones en 1996 y se dedica a la
@@ -187,15 +195,17 @@ export default {
 	background-color: var(--dark-alt);
 }
 
-.links {
+.links{
 	color: #eee;
 	text-decoration: none;
 	font-style: 1.5em;
+	
+}
+.link:hover{
 	background-color: var(--dark);
 	padding: 5px;
 	border-radius: 13px;
 }
-
 .logo-image {
 	width: 50px;
 	height: 50px;
@@ -215,17 +225,17 @@ export default {
 	background-color: var(--dark-alt);
 }
 
-@media (max-width: 1025px) {
-	.component-container {
-		padding: 3px !important;
-	}
-}
 
-.section{
+
+
+
+
+.section {
 	position: absolute;
 	width: 100%;
 	height: 90;
 }
+
 
 </style>
 
